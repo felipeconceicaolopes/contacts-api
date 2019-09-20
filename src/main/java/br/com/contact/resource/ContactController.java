b71cf4014046bba2a907143e0db8489cee1037a4
felipetiburcioDASA <felipe.tiburcio@dasa.com.br>
2019-09-19 21:07:31 -0300

    Adding findAll contacts

--- src/main/java/br/com/contact/resource/ContactController.java
+++ src/main/java/br/com/contact/resource/ContactController.java
@@ -1,8 +1,11 @@
 package br.com.contact.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,12 @@ public class ContactController {
 	public ResponseEntity<Contact> save(@RequestBody Contact contact) {		 
 		 
 		 return new ResponseEntity<Contact>(contactService.save(contact), HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Contact>> findAll(){
+		List<Contact> contacts = contactService.getContacts();
+		
+		return ResponseEntity.ok(contacts);
 	}
 }
